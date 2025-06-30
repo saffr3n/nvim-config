81d2334837d66b87fa098305fbc05e0707a20829
saffr3n <saffr3n@gmail.com>
2025-06-30 20:45:28 +0300

    feat: show floating diagnostic on jump
    - remove redundant `virtual_text.format` override

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -78,8 +78,9 @@ return {
 
         vim.diagnostic.config({
           severity_sort = true,
+          jump = { float = true },
           float = { border = 'rounded', source = 'if_many' },
-          underline = { severity = vim.diagnostic.severity.ERROR },
+          virtual_text = { source = 'if_many', spacing = 2 },
           signs = {
             text = {
               [vim.diagnostic.severity.ERROR] = '󰅚 ',
@@ -87,19 +88,6 @@ return {
               [vim.diagnostic.severity.INFO] = '󰋽 ',
               [vim.diagnostic.severity.HINT] = '󰌶 ',
             },
-          },
-          virtual_text = {
-            source = 'if_many',
-            spacing = 2,
-            format = function(diagnostic)
-              local diagnostic_message = {
-                [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                [vim.diagnostic.severity.WARN] = diagnostic.message,
-                [vim.diagnostic.severity.INFO] = diagnostic.message,
-                [vim.diagnostic.severity.HINT] = diagnostic.message,
-              }
-              return diagnostic_message[diagnostic.severity]
-            end,
           },
         })
       end,
