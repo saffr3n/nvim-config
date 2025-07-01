1228e7023ecfc2068d9dc37fe3cf7564d4038166
saffr3n <saffr3n@gmail.com>
2025-07-01 14:43:24 +0300

    feat: add angle bracket support to nvim-autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,14 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  opts = {},
+  config = function()
+    local autopairs = require('nvim-autopairs')
+    local Rule = require('nvim-autopairs.rule')
+    local cond = require('nvim-autopairs.conds')
+
+    autopairs.setup({})
+    autopairs.add_rule(
+      Rule('<', '>'):with_pair(cond.before_regex('%a+')):with_move(function(opts) return opts.char == '>' end)
+    )
+  end,
 }
