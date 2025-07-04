116501bd7a4e2fc40c774ece86a34e7f2916bc20
saffr3n <saffr3n@gmail.com>
2025-07-04 23:12:57 +0300

    feat: adjust blink.cmp setup
    - get rid of snippets
    - disable auto brackets
    - enable docs auto show

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -2,18 +2,16 @@ return {
   'saghen/blink.cmp',
   event = 'VimEnter',
   version = '1.*',
-  dependencies = {
-    'rafamadriz/friendly-snippets',
-    'folke/lazydev.nvim',
-  },
+  dependencies = { 'folke/lazydev.nvim' },
   opts = {
     keymap = { preset = 'default' },
     appearance = { nerd_font_variant = 'mono' },
     completion = {
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      accept = { auto_brackets = { enabled = false } },
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
     },
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
+      default = { 'lsp', 'path', 'buffer', 'lazydev' },
       providers = {
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
       },
