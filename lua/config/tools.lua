c8576f397d16c7df55e01e0796e5eee4a65e9f73
saffr3n <saffr3n@gmail.com>
2025-08-19 21:16:43 +0300

    feat: add dap with javascript debugger

--- lua/config/tools.lua
+++ lua/config/tools.lua
@@ -6,6 +6,31 @@ local lsp_servers = {
   emmet_language_server = {},
 }
 
+local dap = {
+  ['js-debug-adapter'] = function()
+    local dap = require('dap')
+
+    dap.adapters['pwa-node'] = {
+      type = 'server',
+      host = 'localhost',
+      port = '${port}',
+      executable = { command = 'js-debug-adapter', args = { '${port}' } },
+    }
+
+    for _, lang in ipairs({ 'javascript', 'typescript' }) do
+      dap.configurations[lang] = {
+        {
+          type = 'pwa-node',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+        },
+      }
+    end
+  end,
+}
+
 local null_tools = {
   cspell = function()
     local cspell = require('cspell')
@@ -30,12 +55,20 @@ local null_tools = {
 }
 
 local all_tools = vim.tbl_keys(lsp_servers)
+vim.list_extend(all_tools, vim.tbl_keys(dap))
 vim.list_extend(all_tools, vim.tbl_keys(null_tools))
 
 return {
   nvim_lspconfig = {
     servers = lsp_servers,
     ensure_installed = all_tools,
+  },
+  dap = {
+    setup_debuggers = function()
+      for _, setup in pairs(dap) do
+        setup()
+      end
+    end,
   },
   null_ls = {
     dependencies = { 'davidmh/cspell.nvim' },
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,77 @@
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'mason-org/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+    'theHamsta/nvim-dap-virtual-text',
+  },
+  keys = {
+    {
+      '<F5>',
+      function() require('dap').continue() end,
+    },
+    {
+      '<F1>',
+      function() require('dap').step_into() end,
+    },
+    {
+      '<F2>',
+      function() require('dap').step_over() end,
+    },
+    {
+      '<F3>',
+      function() require('dap').step_out() end,
+    },
+    {
+      '<leader>b',
+      function() require('dap').toggle_breakpoint() end,
+    },
+    {
+      '<leader>B',
+      function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
+    },
+    {
+      '<F7>',
+      function() require('dapui').toggle() end,
+    },
+  },
+  config = function()
+    local dap = require('dap')
+    local dapui = require('dapui')
+
+    require('nvim-dap-virtual-text').setup({})
+
+    require('mason-nvim-dap').setup({
+      automatic_installation = true,
+      handlers = {},
+      ensure_installed = {},
+    })
+
+    ---@diagnostic disable-next-line: missing-fields
+    dapui.setup({
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      ---@diagnostic disable-next-line: missing-fields
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    })
+
+    dap.listeners.after.event_initialized.dapui_config = dapui.open
+    dap.listeners.before.event_terminated.dapui_config = dapui.close
+    dap.listeners.before.event_exited.dapui_config = dapui.close
+
+    require('config.tools').dap.setup_debuggers()
+  end,
+}
