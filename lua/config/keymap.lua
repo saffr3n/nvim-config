317e955be15e323e7a8d8d4c058bac0d8a8eff0d
saffr3n <saffr3n@gmail.com>
2025-11-22 19:38:43 +0300

    feat: remove unused keymaps
    - Use Snacks.picker.diagnostics and Snacks.picker.diagnostics_buffer
    instead of loclist
    - Snacks.terminal has <Esc><Esc> out of the box

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -2,7 +2,3 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<Esc>', vim.cmd.nohlsearch)
-
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
