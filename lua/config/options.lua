9a099be82fcead981d34caeeb46567cd5ac1390f
saffr3n <saffr3n@gmail.com>
2025-11-25 21:58:43 +0300

    feat: adjust options
    - remove vim.o.mouse
    - set vim.o.statuscolumn to '%l%s%C'
    - set vim.o.laststatus to 3
    - set vim.o.windorder to 'rounded'
    - set vim.o.pumborder to 'rounded'
    - set vim.o.shiftwidth to 2
    - set vim.o.softtabstop to -1
    - set vim.o.expandtab to true
    - change vim.opt.listchars with vim.o.listchars and simplify it
    - set vim.o.fillchars to make fold column pretty
    - remove vim.o.updatetime
    - remove vim.o.timeoutlen
    - unwrap vim.o.clipboard
    - rename hl-yank to yank-hl for consistency
    - add vim.o.completeopt and vim.o.wildmode for future when in-built
    neovim completion don't suck

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,3 @@
-vim.o.mouse = 'a'
-
 vim.o.number = true
 vim.o.relativenumber = true
 
@@ -11,7 +9,12 @@ vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.o.foldlevel = 99
 vim.o.foldtext = ''
 
+vim.o.statuscolumn = '%l%s%C'
+vim.o.laststatus = 3
 vim.o.showmode = false
+
+vim.o.winborder = 'rounded'
+vim.o.pumborder = 'rounded'
 
 vim.o.inccommand = 'split'
 
@@ -21,11 +24,16 @@ vim.o.smartcase = true
 vim.o.confirm = true
 vim.o.backupcopy = 'yes'
 
+vim.o.shiftwidth = 2
+vim.o.softtabstop = -1
+vim.o.expandtab = true
+
 vim.o.breakindent = true
 vim.o.linebreak = true
 
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.listchars = 'trail:·,nbsp:·'
+vim.o.fillchars = 'fold: ,foldopen:,foldclose:,foldsep: ,foldinner: '
 
 vim.o.cursorline = true
 
@@ -36,12 +44,12 @@ vim.o.splitbelow = true
 
 vim.o.undofile = true
 
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.o.clipboard = 'unnamedplus'
 
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+-- vim.o.completeopt = 'menuone,noselect,fuzzy,popup'
+-- vim.o.wildmode = 'noselect:lastused,full'
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('hl-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('yank-hl', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
