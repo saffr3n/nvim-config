0a8cabec9f0b439bef354f9dca29250181183c65
saffr3n <saffr3n@gmail.com>
2025-11-25 21:35:23 +0300

    feat: remove none-ls

--- lua/config/tools.lua
+++ lua/config/tools.lua
@@ -32,22 +32,8 @@ local dap = {
   end,
 }
 
-local null_tools = {
-  eslint_d = function()
-    return {
-      require('none-ls.diagnostics.eslint_d'),
-      require('none-ls.code_actions.eslint_d'),
-    }
-  end,
-
-  prettierd = function() return { require('null-ls').builtins.formatting.prettierd } end,
-
-  stylua = function() return { require('null-ls').builtins.formatting.stylua } end,
-}
-
 local all_tools = vim.tbl_keys(lsp_servers)
 vim.list_extend(all_tools, vim.tbl_keys(dap))
-vim.list_extend(all_tools, vim.tbl_keys(null_tools))
 
 return {
   nvim_lspconfig = {
@@ -59,16 +45,6 @@ return {
       for _, setup in pairs(dap) do
         setup()
       end
-    end,
-  },
-  null_ls = {
-    dependencies = { 'davidmh/cspell.nvim' },
-    get_sources = function()
-      local sources = {}
-      for _, get in pairs(null_tools) do
-        vim.list_extend(sources, get())
-      end
-      return sources
     end,
   },
 }
