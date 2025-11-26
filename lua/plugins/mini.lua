fd2c9d37a2d549ad5b5a1c03b0273ec4cb3a18b7
saffr3n <saffr3n@gmail.com>
2025-11-26 16:03:11 +0300

    feat: enable mini.git

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,6 +4,7 @@ return {
     require('mini.icons').setup()
     require('mini.ai').setup()
     require('mini.surround').setup()
+    require('mini.git').setup()
     require('mini.pairs').setup({
       mappings = {
         ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[%w_]' },
@@ -11,13 +12,13 @@ return {
       },
     })
 
+    local diff = require('mini.diff')
+    require('config.utils').map('n', '<Leader>tg', diff.toggle_overlay)
+    diff.setup()
+
     local statusline = require('mini.statusline')
     ---@diagnostic disable-next-line: duplicate-set-field
     statusline.section_location = function() return '%l:%v' end
     statusline.setup()
-
-    local diff = require('mini.diff')
-    require('config.utils').map('n', '<Leader>tg', diff.toggle_overlay)
-    diff.setup()
   end,
 }
