870b151fd7555026c063fc0dccc9207f225fa49b
saffr3n <saffr3n@gmail.com>
2025-11-27 16:09:16 +0300

    feat: disable treesitter indent
    doesn't seem to do anything for me

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,10 +12,6 @@ return {
   opts = {
     ensure_installed = require('config.tools').ensure_installed.treesitter,
     auto_install = false,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
   },
 }
