fd59b6ef1e5e78f4ad8dfcca0022045822997e15
saffr3n <saffr3n@gmail.com>
2025-11-27 16:13:21 +0300

    feat: adjust colorscheme
    - set PmenyBorder and WinSeparator to be same with FloatBorder
    - highlight CursorLineFold
    - make bg transparent by default
    - create keymap to toggle bg

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -5,12 +5,27 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    ---@diagnostic disable-next-line: missing-fields
+    local bg ---@type (string|integer)?
+    local function toggle_bg()
+      local hl = vim.api.nvim_get_hl(0, { name = 'Normal' }) --[[@as vim.api.keyset.highlight]]
+      hl.bg = not hl.bg and bg or nil
+      vim.api.nvim_set_hl(0, 'Normal', hl)
+      vim.api.nvim_set_hl(0, 'NormalNC', hl)
+    end
+
     require('tokyonight').setup({
-      styles = {
-        comments = { italic = false },
-      },
+      style = 'night',
+      styles = { comments = { italic = false } },
+      on_highlights = function(hl)
+        bg = hl.Normal.bg
+        hl.PmenuBorder = 'FloatBorder'
+        hl.WinSeparator = 'FloatBorder'
+        hl.CursorLineFold = 'Constant'
+      end,
     })
-    vim.cmd.colorscheme('tokyonight-night')
+
+    vim.cmd.colorscheme('tokyonight')
+    require('config.utils').map('n', '<Leader>tb', toggle_bg)
+    toggle_bg()
   end,
 }
