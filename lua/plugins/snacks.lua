507023a50c8a02ae585acd59be32068cc9e37208
saffr3n <saffr3n@gmail.com>
2025-11-27 16:25:26 +0300

    feat: add Mason and Lazygit sections to dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,7 +7,22 @@ return {
   ---@module 'snacks'
   ---@type snacks.Config
   opts = {
-    dashboard = {},
+    dashboard = {
+      preset = {
+        keys = {
+          { icon = ' ', key = 'f', desc = 'Find File', action = ':lua Snacks.dashboard.pick("files")' },
+          { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+          { icon = ' ', key = 'g', desc = 'Find Text', action = ':lua Snacks.dashboard.pick("live_grep")' },
+          { icon = ' ', key = 'r', desc = 'Recent Files', action = ':lua Snacks.dashboard.pick("oldfiles")' },
+          { icon = ' ', key = 'c', desc = 'Config', action = ':lua Snacks.dashboard.pick("files", { cwd = vim.fn.stdpath("config") })' },
+          { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+          { icon = ' ', key = 'l', desc = 'Lazygit', action = ':lua Snacks.lazygit.open()' },
+          { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+          { icon = '󰰎 ', key = 'M', desc = 'Mason', action = ':Mason' },
+          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+        },
+      },
+    },
     explorer = {},
     indent = {},
     input = {},
